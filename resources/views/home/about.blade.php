91377347cdbc60ee4972f8909728b835a2158bce
Suvarnanand0508 <rajaneedu2001@gmail.com>
2024-11-13 17:33:35 -0600

    Update about.blade.php

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -1,15 +1,14 @@
 <section class="about" id="about">
     <div class="max-width">
-        <h2 class="title">About Delicious Eats </h2>
-        <div class="about-content">
-            <div class="column left">
-                <img style="width: 500px" src="images/download.jpeg" alt="">
+        <h2 class="title">About GRABNGO </h2>
+        <div class="about-content" style="display: flex; align-items: center;">
+            <div class="column left" style="flex: 1;">
+                <img style="width: 500px" src="images/download.jpeg" alt="" style="width: 100%; max-width: 500px;">
             </div>
-            <div class="column right">
-                <div class="text" style="color: black">HELLO <span class="typing-2"></span></div>
-                <p style="color: black">About Delicious Eats delivers the finest in gourmet cuisine and culinary delights. Discover exclusive, mouthwatering dishes that blend innovative flavors with classic recipes, all curated for the discerning palate.
-                </p>
-                <a href="">SUBSRIBE TO YOUTUBE</a>
+            <div class="column right" style="flex: 1; padding-left: 20px;">
+            <div class="text" style="font-size: 24px; font-weight: bold; color: black;">HELLO</div>
+            <p style="color: black; font-size: 16px; line-height: 1.6;">
+            GRABNGO is the go-to platform for UNT students and faculty to effortlessly order food from campus dining spots. Explore menu options, place your order online, and pick up at your convenience.</p>
             </div>
         </div>
     </div>
