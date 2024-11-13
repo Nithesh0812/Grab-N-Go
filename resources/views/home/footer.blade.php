b5cf34dbb3d6abdb0e7ab8d6bb90d5b2bd716877
Suvarnanand0508 <rajaneedu2001@gmail.com>
2024-11-13 17:34:48 -0600

    Update footer.blade.php

--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -10,7 +10,7 @@
     }
 </style>
 <div style="background: #484545" class="cpy_" class="footer">
-    <p class="mx-auto">© 2022 All Rights Reserved By <a href="/">DELICIOUS EATS</a><br>
+    <p class="mx-auto">© 2022 All Rights Reserved By <a href="/">GRABNGO</a><br>
 
         Distributed By <a href="/" target="_blank">project manager</a>
 
