b131538bdb5e0f63b9a7807c0fc20bb9c4f872e7
Suvarnanand0508 <rajaneedu2001@gmail.com>
2024-11-13 17:39:19 -0600

    Update slider.blade.php

--- resources/views/home/slider.blade.php
+++ resources/views/home/slider.blade.php
@@ -2,7 +2,7 @@
     <div class="max-width">
         <div class="home-content">
             <div class="text-1" style="font-size: 1.2rem">Satisfy Your Cravings with, </div>
-            <div class="text-2">DELICIOUS EATS</div>
+            <div class="text-2">GRABNGO</div>
             <div class="text-3">PLACE YOUR ORDER HERE, <span class="typing"></span></div>
         </div>
     </div>
