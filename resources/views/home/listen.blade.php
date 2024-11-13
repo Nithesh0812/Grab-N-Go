339552de048f5da0bce3baef77a0d21dbc92dc07
Suvarnanand0508 <rajaneedu2001@gmail.com>
2024-11-13 17:37:34 -0600

    Update listen.blade.php

--- resources/views/home/listen.blade.php
+++ resources/views/home/listen.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DELICIOUS EATS</title>
+    <title>GRABNGO</title>
     @include('home.css')
 
 
