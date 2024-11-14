9e227e2ad46fbc3fe0051160abf4b4912ff690e5
Nithesh0812 <82301793+Nithesh0812@users.noreply.github.com>
2024-11-13 22:19:10 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -40,9 +40,9 @@ route::post('/add_product',[AdminController::class,'add_product']);
 
 route::get('/show_product',[AdminController::class,'show_product']);
 
-route::get('/send_email/{id}',[AdminController::class,'send_email']);
-
 route::get('/delete_product/{id}',[AdminController::class,'delete_product']);
+
+route::get('/send_email/{id}',[AdminController::class,'send_email']);
 
 route::get('/update_product/{id}',[AdminController::class,'update_product']);
 
