c211a4b298b9e953e45e32ca9af32fc183032cdd
Sravani Meka <69510620+Sravani2511@users.noreply.github.com>
2024-11-22 02:48:14 -0600

    Update 2022_11_02_040741_create_orders_table.php

--- database/migrations/2022_11_02_040741_create_orders_table.php
+++ database/migrations/2022_11_02_040741_create_orders_table.php
@@ -27,6 +27,7 @@ return new class extends Migration
             $table->string('price')->nullable();
             $table->string('image')->nullable();
             $table->string('product_id')->nullable();
+            $table->string('pick_up_time')->nullable();
 
             $table->string('payment_status')->nullable();
             $table->string('delivery_status')->nullable();
