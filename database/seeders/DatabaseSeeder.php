aec8a045bf2a4e194f72b340f0d35833fb7df88f
Sravani Meka <69510620+Sravani2511@users.noreply.github.com>
2024-12-02 00:19:53 -0600

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,18 +4,34 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      *
-     * @return void
+     *
      */
     public function run()
     {
-        $this->call(CampusRestaurantSeeder::class);
+        $sqlFilePath = database_path('seeders/sql/script.sql');
 
+        if (file_exists($sqlFilePath)) {
+            // Read the SQL file
+            $sql = file_get_contents($sqlFilePath);
+
+            try {
+                // Run the SQL file
+                DB::unprepared($sql);
+                Log::info('Successfully seeded campuses and restaurants from SQL file.');
+            }
+            catch(\Exception $e){
+                  Log::error('Error seeding from SQL file: ' . $e->getMessage());
+            }}
+
+        Log::info('Seeding completed successfully.');
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
